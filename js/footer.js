1a43bdc3b863559b0a060e499d4b00fe810519e5
Arthur Fuller <alfulleracc@gmail.com>
2021-03-25 15:42:34 -0700

    v2

--- js/footer.js
+++ js/footer.js
@@ -12,13 +12,14 @@ let footer = () => {
             <li><a href="index.html">Home</a></li>
             <li><a href="about.html">About</a></li>
             <li><a href="talent.html">Talent</a></li>
-            <li><a href="virtual.html">Virtual</a></li>
+            <li><a href="services.html">Services</a></li>xwx
         </ul>
     </nav>
     <nav>
-        <ul>                    
-            <li><a href="services.html">Services</a></li>
+        <ul>
+            <li><a href="virtual.html">Virtual</a></li>                   
             <li><a href="clients.html">Clients</a></li>
+            <li><a href="contact.html">Contact</a></li>
         </ul>
     </nav>
         <div class="social">
@@ -29,6 +30,3 @@ let footer = () => {
     </div>`;
     $('footer').html(divFooter);
 }
-
-
-
--- js/nav.js
+++ js/nav.js
@@ -7,9 +7,10 @@ let nav = () => {
                 <li><a href="index.html">Home</a></li>
                 <li><a href="about.html">About</a></li>
                 <li><a href="talent.html">Talent</a></li>
-                <li><a href="virtual.html">Virtual</a></li>
                 <li><a href="services.html">Services</a></li>
+                <li><a href="virtual.html">Virtual</a></li>
                 <li><a href="clients.html">Clients</a></li>
+                <li><a href="contact.html">Contact</a></li>
             </ul>
         </nav>
     </div>`
