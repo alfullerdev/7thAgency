35ad55150767bbf7d911958b86a48aa1a55d337d
Arthur Leon Fuller <52759634+alfullerdev@users.noreply.github.com>
2022-11-03 18:38:18 -0700

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -8,7 +8,6 @@ let nav = () => {
                 <li><a href="about.html">About</a></li>
                 <li><a href="talent.html">Talent</a></li>
                 <li><a href="services.html">Services</a></li>
-                <li><a href="virtual.html">Virtual</a></li>
                 <li><a href="clients.html">Clients</a></li>
                 <li><a href="contact.html">Contact</a></li>
             </ul>
